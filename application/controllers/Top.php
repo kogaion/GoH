f31f037a9c8bbc65156a7c1d70ad5566659282aa
Lucian Toza <lucian.toza@avangate.com>
2016-05-14 18:21:21 +0300

    Fix TopModel.

--- application/controllers/Top.php
+++ application/controllers/Top.php
@@ -7,10 +7,11 @@ class Top extends CI_Controller
 
     public function index()
     {
-        $this->load->model('topmodel');
+        //$this->load->model('topmodel');
 
 
-        $results = $this->topmodel->getTopUsers( date("Y-m-d H:i:s",  strtotime("-1 week") ) , date("Y-m-d H:i:s", time()), 1000, 'DESC');
+        $results = $this->TopModel->getTopUsers( date("Y-m-d H:i:s",  strtotime("-1 week") ) , date("Y-m-d H:i:s", time()) );
+        
 
         $this->data['best'] = [];
         foreach ( $results as $result ) {
@@ -27,7 +28,7 @@ class Top extends CI_Controller
         }
 //
 //
-//        $results = $this->topmodel->getTopUsers( date("Y-m-d H:i:s",  strtotime("-1 week") ) , date("Y-m-d H:i:s", time()), 3, 'ASC');
+//        $results = $this->TopModel->getTopUsers( date("Y-m-d H:i:s",  strtotime("-1 week") ) , date("Y-m-d H:i:s", time()), 3, 'ASC');
 //
 //        $this->data['worst'] = [];
 //        foreach ( $results as $result ) {
@@ -48,8 +49,8 @@ class Top extends CI_Controller
 
     public function test()
     {
-        $this->load->model('topmodel');
-        var_dump($this->topmodel->getTopProjects('2016-05-14 12:30:12', '2016-05-14 12:31:15', 3));
+        //$this->load->model('topmodel');
+        var_dump($this->TopModel->getTopProjects('2016-05-14 12:30:12', '2016-05-14 12:31:15', 3));
     }
 
 }
